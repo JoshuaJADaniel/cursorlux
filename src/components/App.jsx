6d18dee80200e7ce418dd88a5782dc927d96600e
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-28 18:42:05 -0500

    Utilize new panels and set up navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Header from "components/Header";
+import ClickPanel from "components/ClickPanel";
+import BorderPanel from "components/BorderPanel";
+import BackgroundPanel from "components/BackgroundPanel";
+import { AppBar, Tabs, Tab } from "@material-ui/core";
 
-const App = () => <Header />;
+export default function SimpleTabs() {
+  const [currentTab, setCurrentTab] = useState(0);
 
-export default App;
+  const switchTab = (event, newValue) => {
+    setCurrentTab(newValue);
+  };
+
+  return (
+    <div>
+      <Header />
+      <AppBar position="static">
+        <Tabs variant="fullWidth" value={currentTab} onChange={switchTab}>
+          <Tab label="Click" />
+          <Tab label="Background" />
+          <Tab label="Border" />
+        </Tabs>
+      </AppBar>
+      <ClickPanel index={0} value={currentTab} />
+      <BackgroundPanel index={1} value={currentTab} />
+      <BorderPanel index={2} value={currentTab} />
+    </div>
+  );
+}
