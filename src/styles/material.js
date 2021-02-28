65c54b08aadb197992309f4751cca8a7be26f96a
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-28 11:42:41 -0500

    Create material overrides and color exports

---
+++ src/styles/material.js
@@ -0,0 +1,22 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+import colors from "styles/material.module.scss";
+
+const material = createMuiTheme({
+  palette: {
+    primary: {
+      main: colors.primary,
+    },
+    secondary: {
+      main: colors.secondary,
+    },
+  },
+  typography: {
+    fontFamily: ["'Comfortaa'", "sans-serif"].join(","),
+    button: {
+      fontWeight: 700,
+      textTransform: "none",
+    },
+  },
+});
+
+export default material;
