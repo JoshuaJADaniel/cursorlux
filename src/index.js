9c6e70afb31c860454a949f843ecac44c1ccbf17
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-28 15:28:30 -0500

    Include <ThemeProvider /> overrides and main Sass file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "components/App";
+import reportWebVitals from "reportWebVitals";
+import { ThemeProvider } from "@material-ui/styles";
+
+import "styles/main.scss";
+import material from "styles/material";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={material}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
