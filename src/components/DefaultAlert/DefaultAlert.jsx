8d1a00c20941e073ac19190ee3d70a4dc83c4495
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-01 10:59:33 -0500

    Remove overrides for alert spacing (will move to main Sass)

--- src/components/DefaultAlert/DefaultAlert.jsx
+++ src/components/DefaultAlert/DefaultAlert.jsx
@@ -4,14 +4,11 @@ import PropTypes from "prop-types";
 import { Alert } from "@material-ui/lab";
 import { Snackbar } from "@material-ui/core";
 
-import styles from "./DefaultAlert.module.scss";
-
 const DefaultAlert = ({ message, visible, onClose, severity }) => (
   <Snackbar
     open={visible}
     onClose={onClose}
     autoHideDuration={3000}
-    className={styles.alertSpacing}
     anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
   >
     <Alert severity={severity} variant="filled" onClose={onClose}>
