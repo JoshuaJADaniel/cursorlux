1a8d76473cbb65700b44134f18a576cb7755e7bf
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-01 11:14:32 -0500

    Add active state management

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import FullLogo from "assets/full-logo.svg";
-import { FiPauseCircle } from "react-icons/fi";
+import { FiPauseCircle, FiPlayCircle } from "react-icons/fi";
 import { AppBar, IconButton, Toolbar } from "@material-ui/core";
 
 import styles from "./Header.module.scss";
 
-const Header = () => (
-  <AppBar position="static" color="transparent">
-    <Toolbar>
-      <img src={FullLogo} height={24} alt="Mouse Highlighter" />
-      <div className={styles.grow} />
-      <IconButton className={styles.button} edge="end">
-        <FiPauseCircle size={28} strokeWidth={1.5} />
-      </IconButton>
-    </Toolbar>
-  </AppBar>
-);
+const Header = () => {
+  const [active, setActive] = useState(true);
+
+  const handleActive = () => setActive(!active);
+
+  return (
+    <AppBar position="static" color="transparent">
+      <Toolbar>
+        <img src={FullLogo} height={24} alt="Mouse Highlighter" />
+        <div className={styles.grow} />
+        <IconButton className={styles.button} onClick={handleActive} edge="end">
+          {active ? (
+            <FiPauseCircle size={28} strokeWidth={1.5} />
+          ) : (
+            <FiPlayCircle size={28} strokeWidth={1.5} />
+          )}
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Header;
