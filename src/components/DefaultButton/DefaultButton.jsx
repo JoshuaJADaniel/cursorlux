19d0d7ff6dd8a87218512c2b8054591603f40ae5
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-02-28 23:31:47 -0500

    Create <DefaultButton /> component

---
+++ src/components/DefaultButton/DefaultButton.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box, Button } from "@material-ui/core";
+
+const DefaultButton = ({ message, onClick, spaced }) => (
+  <Box m={spaced ? 2 : 0}>
+    <Button size="large" color="primary" variant="contained" onClick={onClick}>
+      {message}
+    </Button>
+  </Box>
+);
+
+DefaultButton.propTypes = {
+  spaced: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+export default DefaultButton;
