b6e4839fe0465d7480ff42a41c8b166eab3ca644
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-02 01:08:55 -0500

    Prevent fading in on load (Chrome already does this)

--- src/components/TabPanel/TabPanel.jsx
+++ src/components/TabPanel/TabPanel.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Box, Fade } from "@material-ui/core";
 
-const TabPanel = ({ index, value, children }) => (
-  <Fade in={value === index} timeout={600}>
-    <Box pt={2} px={2.8} hidden={value !== index}>
-      {children}
-    </Box>
-  </Fade>
-);
+const TabPanel = ({ index, value, children }) => {
+  const [timeout, setTimeout] = useState(0);
+
+  useEffect(() => {
+    setTimeout(600);
+  }, []);
+
+  return (
+    <Fade in={value === index} timeout={timeout}>
+      <Box pt={2} px={2.8} hidden={value !== index}>
+        {children}
+      </Box>
+    </Fade>
+  );
+};
 
 TabPanel.propTypes = {
   index: PropTypes.number.isRequired,
