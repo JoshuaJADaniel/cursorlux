84bb4ac73586f174897191dcc6f9adfb65cd7943
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-02 00:25:30 -0500

    Add option for title text

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Box } from "@material-ui/core";
 import { CirclePicker } from "react-color";
+import { Box, Typography } from "@material-ui/core";
 
 import styles from "./ColorPicker.module.scss";
 
-const ColorPicker = ({ color, onChange }) => (
+const ColorPicker = ({ title, color, onChange }) => (
   <Box my={2}>
-    <CirclePicker
-      color={color}
-      circleSpacing={0}
-      onChange={onChange}
-      className={styles.gridContainer}
-    />
+    <Typography>{title}</Typography>
+    <Box my={2}>
+      <CirclePicker
+        color={color}
+        circleSpacing={0}
+        onChange={onChange}
+        className={styles.gridContainer}
+      />
+    </Box>
   </Box>
 );
 
 ColorPicker.propTypes = {
+  title: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
