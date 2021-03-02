a538ac6493947cea896da6d9f13ebb34f2030e76
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-02 17:36:03 -0500

    Update class name and include chrome storage access/updates

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import FullLogo from "assets/full-logo.svg";
+import { strToBool, boolToStr } from "utils/typeConversions";
 import { FiPauseCircle, FiPlayCircle } from "react-icons/fi";
 import { AppBar, IconButton, Toolbar } from "@material-ui/core";
 
@@ -9,14 +10,29 @@ import styles from "./Header.module.scss";
 const Header = () => {
   const [active, setActive] = useState(true);
 
-  const handleActive = () => setActive(!active);
+  const handleActive = () => {
+    const newActive = !active;
+    chrome.storage.sync.set({ active: boolToStr(newActive) }, () => {
+      setActive(newActive);
+    });
+  };
+
+  useEffect(() => {
+    chrome.storage.sync.get("active", ({ active }) => {
+      setActive(strToBool(active));
+    });
+  }, []);
 
   return (
     <AppBar position="static" color="transparent">
       <Toolbar>
         <img src={FullLogo} height={24} alt="Mouse Highlighter" />
         <div className={styles.grow} />
-        <IconButton className={styles.button} onClick={handleActive} edge="end">
+        <IconButton
+          edge="end"
+          onClick={handleActive}
+          className={styles.buttonOverrides}
+        >
           {active ? (
             <FiPauseCircle size={28} strokeWidth={1.5} />
           ) : (
