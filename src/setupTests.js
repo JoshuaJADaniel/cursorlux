a73cee622d5ef80b2d1748db3477dbb659359523
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-02 20:40:57 -0500

    Create test environment for global chrome object

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,1 +1,3 @@
-import "@testing-library/jest-dom";
+// Since this application uses the global chrome API, this line allows every
+// component test to run without throwing errors due to undefined variables
+global.chrome = {};
