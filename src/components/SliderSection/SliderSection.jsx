ac7b280e2d16eeec08dde7c1733de0b57d78ad49
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-04 13:15:33 -0500

    Add unit tests and rename <SliderSection />

---
+++ src/components/SliderSection/SliderSection.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Box, Slider, Typography } from "@material-ui/core";
+import Flexbox from "../Flexbox";
+
+const SliderSection = ({
+  min,
+  max,
+  step,
+  title,
+  height,
+  value,
+  format,
+  onChange,
+}) => {
+  const onChangeWrapper = (event, value) => {
+    onChange(value);
+  };
+
+  return (
+    <Flexbox height={height || null} alignItems="center">
+      <Box pt={1.5} pb={0.3125} width="100%">
+        <Typography gutterBottom>{title}</Typography>
+        <Slider
+          marks
+          max={max}
+          min={min}
+          step={step}
+          value={value}
+          onChange={onChangeWrapper}
+          valueLabelFormat={format}
+          valueLabelDisplay="auto"
+        />
+      </Box>
+    </Flexbox>
+  );
+};
+
+SliderSection.propTypes = {
+  height: PropTypes.number,
+  max: PropTypes.number.isRequired,
+  min: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  onChange: PropTypes.func.isRequired,
+  format: PropTypes.func.isRequired,
+};
+
+export default SliderSection;
---
+++ src/components/SliderSection/SliderSection.test.jsx
@@ -0,0 +1,26 @@
+import SliderSection from "./SliderSection";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/user-event";
+
+const props = {
+  min: 0,
+  max: 100,
+  step: 20,
+  value: 40,
+  format: (value) => `${value}%`,
+  onChange: (value) => {
+    props.value = value;
+  },
+  title: "Test Slider",
+};
+
+test("Renders the correct content", () => {
+  const { getByText, getByRole } = render(<SliderSection {...props} />);
+
+  getByText(props.title);
+
+  const slider = getByRole("slider");
+  expect(slider.getAttribute("aria-valuemin")).toBe(props.min.toString());
+  expect(slider.getAttribute("aria-valuemax")).toBe(props.max.toString());
+  expect(slider.getAttribute("aria-valuenow")).toBe(props.value.toString());
+});
