1863e8a80a27237411e050516cea770d908a8081
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-04 20:19:14 -0500

    Add seconds as a format option

--- src/utils/formatString.js
+++ src/utils/formatString.js
@@ -1,4 +1,5 @@
 const pixelFormat = (val) => `${val}px`;
 const percentFormat = (val) => `${val}%`;
+const secondFormat = (val) => `${val}s`;
 
-export { percentFormat, pixelFormat };
+export { percentFormat, pixelFormat, secondFormat };
