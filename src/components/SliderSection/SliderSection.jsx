4d43b5055a1f0fb0bbf403902fcc208adb1daf11
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-08 14:47:16 -0500

    Update <FlexBox /> import style

--- src/components/SliderSection/SliderSection.jsx
+++ src/components/SliderSection/SliderSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Box, Slider, Typography } from "@material-ui/core";
-import Flexbox from "../Flexbox";
+import Flexbox from "components/Flexbox";
 
 const SliderSection = ({
   min,
