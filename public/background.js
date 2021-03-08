20a1406e042bbd439619a662917685da4801b163
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-08 15:06:56 -0500

    Set extension defaults for background script

--- public/background.js
+++ public/background.js
@@ -1,1 +1,25 @@
-console.log("background.js");
+chrome.runtime.onInstalled.addListener((details) => {
+  if (details.reason === "install") {
+    chrome.storage.sync.set({
+      active: true,
+      click: {
+        size: 40,
+        opacity: 35,
+        enabled: true,
+        color: "#e91e63",
+      },
+      background: {
+        size: 80,
+        delay: 0.35,
+        opacity: 10,
+        color: "#8bc34a",
+      },
+      border: {
+        thickness: 10,
+        style: "Solid",
+        color: "#ff9800",
+        opacity: 80,
+      },
+    });
+  }
+});
