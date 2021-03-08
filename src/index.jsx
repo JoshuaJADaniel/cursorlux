ac0a12ec86cb24ae87efbe94f9447d29abf5387a
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-08 15:07:55 -0500

    Create redux store and provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import reducers from "reducers";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import "utils/chromeMock";
 import App from "components/App";
 import { ThemeProvider } from "@material-ui/styles";
@@ -8,9 +12,16 @@ import { ThemeProvider } from "@material-ui/styles";
 import "styles/main.scss";
 import material from "styles/material";
 
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <ThemeProvider theme={material}>
-    <App />
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={material}>
+      <App />
+    </ThemeProvider>
+  </Provider>,
   document.getElementById("root")
 );
