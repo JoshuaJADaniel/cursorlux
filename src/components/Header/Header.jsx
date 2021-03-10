a02b566243130d7ae1660e0b374c174b9f61f55a
Joshua J. A. Daniel <joshuajadaniel@gmail.com>
2021-03-10 14:49:31 -0500

    Add tooltip for better UX

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,21 @@ import React, { useEffect, useState } from "react";
 
 import FullLogo from "assets/full-logo.svg";
 import { FiPauseCircle, FiPlayCircle } from "react-icons/fi";
-import { AppBar, IconButton, Toolbar } from "@material-ui/core";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  withStyles,
+  Tooltip as MuiTooltip,
+} from "@material-ui/core";
 
 import styles from "./Header.module.scss";
+
+const Tooltip = withStyles({
+  tooltipPlacementLeft: {
+    margin: "0",
+  },
+})(MuiTooltip);
 
 const Header = () => {
   const [active, setActive] = useState(true);
@@ -32,18 +44,20 @@ const Header = () => {
       <Toolbar>
         <img src={FullLogo} height={24} alt="Logo" />
         <div className={styles.grow} />
-        <IconButton
-          edge="end"
-          onClick={handleActive}
-          className={styles.buttonStyles}
-          aria-label={active ? "Pause" : "Activate"}
-        >
-          {active ? (
-            <FiPauseCircle size={28} role="img" />
-          ) : (
-            <FiPlayCircle size={28} role="img" />
-          )}
-        </IconButton>
+        <Tooltip placement="left" title={active ? "Pause" : "Activate"}>
+          <IconButton
+            edge="end"
+            onClick={handleActive}
+            className={styles.buttonStyles}
+            aria-label={active ? "Pause" : "Activate"}
+          >
+            {active ? (
+              <FiPauseCircle size={28} role="img" />
+            ) : (
+              <FiPlayCircle size={28} role="img" />
+            )}
+          </IconButton>
+        </Tooltip>
       </Toolbar>
     </AppBar>
   );
